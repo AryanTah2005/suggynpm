6ae9e9fdbb4df0d1efa37c74bda6cd3bc2591970
Sugger <87353568+Sugger25e@users.noreply.github.com>
2021-09-13 09:07:30 +0800

    Create embedder.js

---
+++ examples/embedder.js
@@ -0,0 +1,11 @@
+const suggy = require("suggy")
+
+module.exports = {
+   name: "embed",
+   description: "Create embed easily",
+   run: async (client, message, args) => {
+
+
+suggy.embed(message)
+      }
+   }
